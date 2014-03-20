0aaf37b5041ef428bbc3d4835422838044be3fe1
Jeff Moss <jeff.moss@octanner.com>
2014-03-19 20:40:07 -0600

    Switch to 'body' encoding scheme

--- lib/omniauth/strategies/garmin.rb
+++ lib/omniauth/strategies/garmin.rb
@@ -8,6 +8,7 @@ module OmniAuth
       option :name, "garmin"
 
       option :client_options, {
+        scheme: :body,
         site: 'http://connectapitest.garmin.com',
         request_token_path: '/oauth-service-1.0/oauth/request_token',
         access_token_path: '/oauth-service-1.0/oauth/access_token',
